dc3c3169d9863d94313c37d8393161c946bf9cff
Washikokevv27 <washikokevv27@gmail.com>
2020-10-25 02:00:58 +0300

    added models

--- app/config.py
+++ app/config.py
@@ -3,7 +3,7 @@ class Config:
     General configuration parent class
     '''
     #pass
-    NEWS_API_BASE_URL = 'https:api'
+    NEWS_API_BASE_URL ='http://newsapi.org/v2/everything?q={}&apiKey={}' 
 
 
 
