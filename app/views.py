12ec05e22c10260b349c84e7691ea24c505b71cd
Washikokevv27 <washikokevv27@gmail.com>
2020-10-24 20:54:09 +0300

    added html body

--- app/views.py
+++ app/views.py
@@ -2,11 +2,21 @@ from flask import render_template
 from app import app
 
 # Views
-@app.route('/news/<int:news_id>')
-def news(news_id):
+#@app.route('/news/<int:news_id>')
+#def news(news_id):
+def index():
 
     '''
-    View news page function that returns the news details page and its data
+    View root page function that returns the index page and its data
     '''
+    
     # message = 'Hello World'
-    return render_template('news.html', id = news_id)
+    title = 'Home - A place where you find Accurate News Online'
+    return render_template('index.html', title = title)
+
+
+
+
+
+
+
